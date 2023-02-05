2c4960f67b8629d92b77c59dae67c7fb49bf1873
Alex <alexander.n.peters@gmail.com>
2023-02-05 13:32:56 -0700

    Set default viewport width

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,6 +6,7 @@ export default () => (
   <>
     <Head>
       <link rel="stylesheet" href="/styles/index.css" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <Radar />
     <AddLightButton />
