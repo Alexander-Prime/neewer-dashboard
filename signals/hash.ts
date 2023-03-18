272b3fac65fdb87bc446aca712d333d0187b6b04
Alex <alexander.n.peters@gmail.com>
2023-03-18 12:14:41 -0700

    Fix type error on hashchange

--- signals/hash.ts
+++ signals/hash.ts
@@ -4,7 +4,9 @@ export const hash = signal(globalThis.location?.hash.slice(1));
 
 addEventListener(
   "hashchange",
-  () => hash.value = globalThis.location?.hash.slice(1),
+  () => {
+    hash.value = globalThis.location?.hash.slice(1);
+  },
 );
 
 effect(() => {
